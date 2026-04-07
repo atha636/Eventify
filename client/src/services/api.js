3a045b273143d775aac676b6de03e200d916af70
Atharv Patidar <atharvpatidar2005@gmail.com>
2026-04-07 14:43:31 +0530

    update api for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL  || "http://localhost:5000/api",
+  withCredentials: true, // ✅ needed if using cookies (safe to keep)
 });
 
-// ✅ Auto-attach token to every request
+// ✅ Auto-attach token
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
